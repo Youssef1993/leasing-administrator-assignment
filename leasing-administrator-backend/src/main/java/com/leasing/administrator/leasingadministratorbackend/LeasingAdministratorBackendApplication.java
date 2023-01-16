8a25bf6769b4a2a4171ff1130dc59f3ec30f0008
Youssef Abidi <jobs.youssef@gmail.com>
2023-01-16 20:27:12 +0100

    initiated project

---
+++ leasing-administrator-backend/src/main/java/com/leasing/administrator/leasingadministratorbackend/LeasingAdministratorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.leasing.administrator.leasingadministratorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeasingAdministratorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeasingAdministratorBackendApplication.class, args);
+    }
+
+}
